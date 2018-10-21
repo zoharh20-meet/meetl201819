da74c75b9f110e8e6d6cd37e8ff039a9dfa02896
zoharh20-meet <zoharh20@meet.mit.edu>
2018-10-21 12:27:51 -0400

    lab 3

---
+++ lab3.py
@@ -0,0 +1,32 @@
+##
+#import turtle
+#for i in range (5):
+#	turtle.pendown ()
+#	turtle.forward (100)
+#	turtle.right (144)
+
+#turtle.mainloop()
+
+###
+#import turtle
+#turtle.begin_fill()
+#
+#turtle.forward (30)
+#turtle.right (90)
+#turtle.forward (20)
+#turtle.right (45)
+#turtle.forward (25)
+#turtle.right (100)
+#turtle.forward (25)
+#turtle.right (35)
+#turtle.forward (20)
+#turtle.end_fill ()
+
+#turtle.mainloop ()
+
+import turtle
+
+turtle.addshape ("moustache.gif")
+turtle.shape ("moustache.gif")
+turtle.mainloop()
+
